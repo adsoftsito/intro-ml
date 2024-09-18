37ba792cbfff06086bfdb8c201df8a0eb8654f43
adsoftsito <adsoft@live.com.mx>
2024-09-17 20:35:16 -0600

    create hello world

---
+++ dataframe.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+myDataFrame = pd.DataFrame( 
+                    {
+                     'Yes': [50,41, 45, 32, 12, 5], 
+                     'No':  [131,2, 4, 5, 6, 2]
+                    }
+                    )
+print(myDataFrame)
